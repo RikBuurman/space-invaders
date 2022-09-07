dcca8f12a6bbf7087cb16ff751d11d586d7e5963
Rik Buurman <95089288+RikBuurman@users.noreply.github.com>
2022-09-07 12:07:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//parameters vast
+var canvx = 650;
+var canvy = 750;
+
+//parameters changing
+var gamestate = 0;
+
+function setup() {
+  createCanvas(canvx, canvy);
+}
+
+function draw() {
+  background(10);
+  if (gamestate = 0);
+}
