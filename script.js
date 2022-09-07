fcf7535b3b7fdc8d0fdf60fb810d96f9ca1bbb4c
Rik Buurman <95089288+RikBuurman@users.noreply.github.com>
2022-09-07 12:45:20 +0000

    Added game menu, 2 buttons for starting game and opening tutorial, right mouseclick for return to main menu added

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 //parameters vast
 var canvx = 650;
 var canvy = 750;
+var ts = 32;
 
 //parameters changing
-var gamestate = 0;
+let gamestate = 0;
 
 function setup() {
   createCanvas(canvx, canvy);
 }
 
 function draw() {
+ 
   background(10);
-  if (gamestate = 0);
+  fill(200);
+  rect(50, 400, 250, 100);
+  fill(10);
+  rect(60, 410, 230, 80);
+  fill(200);
+  rect(350, 400, 250, 100);
+  fill(10);
+  rect(360, 410, 230, 80);
+  if (gamestate == 1) {
+    background(100)
+    if (mouseButton == RIGHT) {
+      gamestate = 0
+    }
+  }
+  if (gamestate == 2) {
+    background(255)
+    if (mouseButton == RIGHT) {
+      gamestate = 0
+    }
+  }
+}
+
+function mouseClicked() {
+  if (gamestate == 0){
+    if(mouseX < 300 && mouseX > 50){
+      if(mouseY <500 && mouseY > 400){
+        gamestate = 1;
+      }
+    }
+    if(mouseX < 550 && mouseX > 350){
+      if(mouseY < 500 && mouseY > 400){
+        gamestate = 2; 
+      }
+    }
+  }
 }
