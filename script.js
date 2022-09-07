1337f5d61184d4eaa13e7caa02ec8b4d887dba7f
Rik Buurman <95089288+RikBuurman@users.noreply.github.com>
2022-09-07 12:55:18 +0000

    Text added to start and tutorial button, and centered

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //parameters vast
 var canvx = 650;
 var canvy = 750;
-var ts = 32;
+var ts = 70;
 
 //parameters changing
 let gamestate = 0;
@@ -13,24 +13,29 @@ function setup() {
 function draw() {
  
   background(10);
+  textSize(ts);
   fill(200);
   rect(50, 400, 250, 100);
   fill(10);
   rect(60, 410, 230, 80);
+  fill(255);
+  text("Start", 100, 475)
   fill(200);
   rect(350, 400, 250, 100);
   fill(10);
   rect(360, 410, 230, 80);
+  fill(255);
+  text("Tutorial", 360, 475)
   if (gamestate == 1) {
-    background(100)
+    background(100);
     if (mouseButton == RIGHT) {
-      gamestate = 0
+      gamestate = 0;
     }
   }
   if (gamestate == 2) {
-    background(255)
+    background(255);
     if (mouseButton == RIGHT) {
-      gamestate = 0
+      gamestate = 0;
     }
   }
 }
