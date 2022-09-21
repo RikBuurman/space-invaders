a9f96f5f65183cfd15b8a46828e73b5a5b8620e6
Rik Buurman <95089288+RikBuurman@users.noreply.github.com>
2022-09-21 12:49:09 +0000

    Movement player added with arrow keys, multiple animated aliens implemented

--- script.js
+++ script.js
@@ -4,25 +4,44 @@ var canvy = 750;
 
 //parameters changing
 let gamestate = 0;
-var type1s = 0.5;
+var vMult = 1;
 
 //parameters other
+let t1_ = []
+var t1, player;
 let ts1, ts2, ts3, text1, text2, text3, text4, text5, text6, img1, imgplayer1, gif1;
 
 //classes
-class type1{
+class Type1{
   constructor(x, y){
     this.x = x;
     this.y = y;
-    this.vx = type1s;
+    this.vx = 0;
   }
 
   draw(){
-    image(giftype1, this.x, this.y, 44, 32)
-    this.x = this.x + this.vx;
-    if(this.x + this.vx == 0 || this.x + this.vx == canvx-16){
+    let tV = this.vx * vMult;
+    if(this.x + tV < 0 || this.x + tV > canvx-44){
       this.vx *= -1;
-      this.y -= 30;
+      this.y += 30;
+    }
+    image(giftype1, this.x, this.y, 44, 32)
+    this.x += tV;
+  }
+}
+
+class Player{
+  constructor(x, y, vx){
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+  }
+
+  draw(){
+    this.x += this.vx;
+    image(imgplayer1, this.x, this.y, 75, 75);
+    if (this.x >= 0 && this.x + 50 <= 650) {
+      this.x += this.vx;
     }
   }
 }
@@ -47,6 +66,7 @@ function preload() {
 
 function setup() {
   createCanvas(canvx, canvy);
+  newgame()
 }
 
 function draw() {
@@ -54,37 +74,14 @@ function draw() {
     menu()
   }
   if (gamestate == 1) {
-    background(100);
-    textSize(ts2);
-    fill(225);
-    textStyle(ITALIC);
-    text(text3, 10, 20);
-    var t1 = new type1(10, 30);
-    image(imgplayer1, 100, 500, 75, 75);
-    t1.draw()
+    run()
   }
   if (gamestate == 2) {
-    background(10);
-    textSize(ts2);
-    fill(225);
-    textStyle(ITALIC)
-    text(text3, 10, 20)
-    textStyle(NORMAL);
-    textSize(ts1);
-    fill(0,191,255);
-    rect(75,50,500,100);
-    fill(225);
-    text(text2, 200, 125);
-    textSize(ts3);
-    text(text4, 50, 250);
-    textSize(ts3);
-    text(text5, 50, 285);
-    textSize(ts3);
-    text(text6, 50, 320);
+    tutorial()
   }
 }
 
-function menu(){
+function menu() {
   background(10);
   image(img1, 140, 100, 370, 126)
   image(gif1, 199.5, 524.5)
@@ -105,13 +102,64 @@ function menu(){
   
 }
 
+function tutorial() {
+  background(10);
+  textSize(ts2);
+  fill(225);
+  textStyle(ITALIC)
+  text(text3, 10, 20)
+  textStyle(NORMAL);
+  textSize(ts1);
+  fill(0,191,255);
+  rect(75,50,500,100);
+  fill(225);
+  text(text2, 200, 125);
+  textSize(ts3);
+  text(text4, 50, 250);
+  textSize(ts3);
+  text(text5, 50, 285);
+  textSize(ts3);
+  text(text6, 50, 320);
+}
+
+function newgame() {
+  for(let i = 0; i < 9; i++){
+    t1_.push(new Type1([i]*((canvx-44)/9), 30))
+  }
+  t1 = new Type1(10, 30);
+  player = new Player(325, 600, 0)
+}
 function run(){
-  
+  background(100);
+  textSize(ts2);
+  fill(225);
+  textStyle(ITALIC);
+  text(text3, 10, 20);
+  for(let i = 0; i < t1_.length; i++){
+    t1_[i].draw()
+  }
+  //t1.draw()
+  player.draw()
 }
 
 function keyPressed() {
   if (keyCode === ESCAPE) {
     gamestate = 0; 
+  } 
+  if (keyCode === LEFT_ARROW) {
+    player.vx = -2;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    player.vx = 2;
+  }
+}
+
+function keyReleased() {
+  if (keyCode === LEFT_ARROW) {
+    player.vx = 0;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    player.vx = 0;
   }
 }
 
